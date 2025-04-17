8a12d18b997141dd0431d898fca5cc101465ddbb
NikolaKaplin <kaplinkola0@gmail.com>
2025-04-18 01:45:29 +0300

    perekrestok

--- sites/perekrestok.ts
+++ sites/perekrestok.ts
@@ -1,8 +1,24 @@
 import axios from "axios";
+import { createObjectCsvWriter } from "csv-writer";
 import { chromium, Page } from "playwright";
+// import { csvWriter } from "..";
+
+export const csvWriter = createObjectCsvWriter({
+  path: "producs.csv",
+  header: [
+    { id: "date", title: "Дата" },
+    { id: "network", title: "Сеть" },
+    { id: "address", title: "Адрес" },
+    { id: "category", title: "Категория" },
+    { id: "sku", title: "SKU" },
+    { id: "price", title: "Цена" },
+  ],
+  encoding: "utf8",
+});
 
 const baseUrl = "https://www.perekrestok.ru/";
-const setStoreUrl = "https://www.perekrestok.ru/api/customer/1.4.1.0/delivery/mode/pickup/";
+const setStoreUrl =
+  "https://www.perekrestok.ru/api/customer/1.4.1.0/delivery/mode/pickup/";
 const storesUrl = "https://www.perekrestok.ru/api/customer/1.4.1.0/shop/points";
 const categoriesUrl =
   "https://www.perekrestok.ru/api/customer/1.4.1.0/catalog/category/1/full";
@@ -145,11 +161,16 @@ async function getAllStores(accessToken: string, cookieString: string) {
       Cookie: `${cookieString}`,
     },
   };
-  const stores = (await axios.request(options)).data.content.items.map((store) => {
-    storeId: store.id,
-    storeLocation: store.location,
-  });
-  // console.log(stores);
+  const stores = (await axios.request(options)).data.content.items.map(
+    (store) => ({
+      storeId: store.id,
+      storeLocation: {
+        lattitude: store.location.coordinates[0],
+        longitude: store.location.coordinates[1],
+      },
+    })
+  );
+
   return stores;
 }
 
@@ -182,18 +203,24 @@ async function getCategories(accessToken: string, cookieString: string) {
 
   const excludedTitles = ["Вино", "Игристые вина", "Пиво"];
 
-  categories = categories.map((child) => ({
-    title: child.category.title,
-    id: child.category.id
-  })).filter((item) => !excludedTitles.includes(item.title));
+  categories = categories
+    .map((child) => ({
+      title: child.category.title,
+      id: child.category.id,
+    }))
+    .filter((item) => !excludedTitles.includes(item.title));
 
   return categories;
 }
 
-async function setStore(storeId: number, accessToken: string, cookieString: string) {
+async function setStore(
+  storeId: number,
+  accessToken: string,
+  cookieString: string
+) {
+  const url = setStoreUrl + storeId;
   const options = {
     method: "PUT",
-    url: setStoreUrl + storeId,
     headers: {
       accept: "application/json, text/plain, */*",
       "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
@@ -213,16 +240,108 @@ async function setStore(storeId: number, accessToken: string, cookieString: stri
       Cookie: `${cookieString}`,
     },
   };
-  const storeEnable = await axios.request(options)
-  return storeEnable.data
+  const storeEnable = await fetch(url, options).then((res) => res.json());
+  console.log(storeEnable);
+  return storeEnable.content.shop;
+}
+
+async function getProducts(
+  categoryId: number,
+  accessToken: string,
+  cookieString: string
+) {
+  const options = {
+    method: "POST",
+    url: "https://www.perekrestok.ru/api/customer/1.4.1.0/catalog/product/feed",
+    headers: {
+      "content-type": "application/json",
+      accept: "application/json, text/plain, */*",
+      "accept-language": "ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7",
+      auth: `Bearer ${accessToken}`,
+      "cache-control": "no-cache",
+      pragma: "no-cache",
+      priority: "u=1, i",
+      "sec-ch-ua":
+        '"Google Chrome";v="135", "Not-A.Brand";v="8", "Chromium";v="135"',
+      "sec-ch-ua-mobile": "?0",
+      "sec-ch-ua-platform": '"Windows"',
+      "sec-fetch-dest": "empty",
+      "sec-fetch-mode": "cors",
+      "sec-fetch-site": "same-origin",
+      "user-agent":
+        "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/135.0.0.0 Safari/537.36",
+      Cookie: `${cookieString}`,
+    },
+    data: {
+      page: 1,
+      perPage: 48,
+      filter: { category: categoryId, onlyWithProductReviews: false },
+      withBestProductReviews: false,
+    },
+  };
+  const products = await axios.request(options);
+  return products.data.content.items.flatMap((item) => ({
+    title: item.title,
+    price: item.priceTag.price.toString().slice(0, -2),
+  }));
+}
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
 }
 
 export async function getPerecrestok() {
   const cookies = await getCookies(baseUrl, true);
-  const stores = await getAllStores(cookies!.accessToken, cookies!.cookieString);
-  const categories = await getCategories(cookies!.accessToken, cookies!.cookieString);
+  const stores = await getAllStores(
+    cookies!.accessToken,
+    cookies!.cookieString
+  );
+  const categories = await getCategories(
+    cookies!.accessToken,
+    cookies!.cookieString
+  );
   for (let i = 0; i < stores.length; i++) {
-    await setStore(stores[i].)
+    const startShop = Date.now();
+    const enableStore = await setStore(
+      stores[i].storeId,
+      cookies!.accessToken,
+      cookies!.cookieString
+    );
+    await Promise.all(
+      categories.map(async (category) => {
+        const products = await getProducts(
+          category.id,
+          cookies!.accessToken,
+          cookies!.cookieString
+        );
+        const records = products.map((product) => ({
+          date: new Date().toISOString(),
+          network: "Перекресток",
+          address: enableStore.address,
+          category: category.title,
+          sku: product.title,
+          price: product.price,
+        }));
+        await csvWriter.writeRecords(records);
+      })
+    );
+    if ((i + 1) % 10 === 0) {
+      console.log("Waiting for 25 seconds after 10 iterations...");
+      await delay(30000);
+    }
+    console.log(`a store  fetched is ${Date.now() - startShop}ms`);
   }
-
 }
+
+// getPerecrestok();
+
+async function collectionCookie() {
+  let resultArr: any = [];
+  for (let i = 0; i < 10; i++) {
+    const cookieString = await getCookies(baseUrl, false);
+    console.log(cookieString);
+    let result = {};
+  }
+}
+collectionCookie();
+//доделать ротацию cookie
